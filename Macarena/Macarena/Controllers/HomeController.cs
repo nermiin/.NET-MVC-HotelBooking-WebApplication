902d0c5d0f572abfd18f3fd4bf5b96935d51cf75
Nermin Kaya <najwa.hasan@ogr.sakarya.edu.tr>
2018-12-02 11:57:50 +0300

    delete EF DB fitst for using EF Code-First

--- Macarena/Macarena/Controllers/HomeController.cs
+++ Macarena/Macarena/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace MACARENA.Controllers
 {
@@ -18,7 +14,7 @@ namespace MACARENA.Controllers
         {
             return View();
         }
-      
+
 
         [Route("turizm")]
         public ActionResult Turizim()
